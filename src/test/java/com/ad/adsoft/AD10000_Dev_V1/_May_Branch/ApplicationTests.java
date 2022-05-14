fdb575f1e39b961566f411b87e9e0f91df4dbbdc
adevara <ashoka.devara2021@gmail.com>
2022-05-14 15:43:57 +0530

    created new project AD10000_Dev_V1.0_May_Branch and added updated files

---
+++ src/test/java/com/ad/adsoft/AD10000_Dev_V1/_May_Branch/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ad.adsoft.AD10000_Dev_V1._May_Branch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
